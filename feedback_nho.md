c0fe072ac4484b44f9189667ca8af11c80a636f7
Nicholas Olds <nolds9@yahoo.com>
2015-11-06 12:16:13 -0500

    NHO: add feedback for DT project 1

---
+++ feedback_nho.md
@@ -0,0 +1,28 @@
+# Project Feedback
+
+## Project Workflow
+
+>Did you complete the user stories, wireframes, task tracking, and/or ERDs, as specified above? Did you use source control as expected for the phase of the program you’re in (detailed above)?
+
+**Meets Expectations**. Nice job meeting an achievable scope for the first project. At only 6 commits, I would've liked to see your original sketches, ideas for how the game would look. As we move forward for larger projects, it would be nice to have more thoughts from your planning process, such as a Trello board, and relevant wireframes.
+
+## Technical Requirements
+
+>Did you deliver a project that met all the technical requirements? Given what the class has covered so far, did you build something that was reasonably complex?
+
+**Meets Expectations**. Congratulations, your project shows prolific use of the technologies and concepts we have covered so far! You clearly identified the problem and came up with a functional game in 65 lines of code, pretty sweet! I really like your creativity with the design, and overall layout!
+
+## Code Quality
+
+>Did you follow code style guidance and best practices covered in class, such as spacing, modularity, and semantic naming? Did you comment your code as your instructors have in class?
+
+**Meets Expectations**. Nice and clean! Html is clearly structured, CSS looks good, and JS works! Moving forward, I recommend looking for ways to make your code more modular and also would like to see you take advantage of libraries that make our lives easier as developers, such as jQuery.
+
+ Please review my [inline code comments here](https://github.com/dennizzy/project1/compare/master...nolds9:feedback) (prefixed with my initials - "NHO").
+
+
+## Problem Solving
+
+>Are you able to defend why you implemented your solution in a certain way? Can you demonstrate that you thought through alternative implementations?
+
+**Meets Expectations** Nice work on this project! In general, I would like to see more evidence of your struggle through a problem, whether that be through more user stories, code comments, or more commits.
--- script.js
+++ script.js
@@ -1,61 +1,62 @@
-var gameArray = ['Ne','Ne','Ti','Ti','Na','Na','Fe','Fe','Ag','Ag','Ca','Ca','Mg','Mg','He','He','Cu','Cu'];
+var gameArray = ['Ne', 'Ne', 'Ti', 'Ti', 'Na', 'Na', 'Fe', 'Fe', 'Ag', 'Ag', 'Ca', 'Ca', 'Mg', 'Mg', 'He', 'He', 'Cu', 'Cu'];
 var cardFlip = 0;
-var gameId = [];
-var gameValue = [];
-Array.prototype.boardShuffle = function(){
-    var i = this.length, j, temp;
-    while(--i > 0){
-        j = Math.floor(Math.random() * (i+1));
-        temp = this[j];
-        this[j] = this[i];
-        this[i] = temp;
+var gameId = []; // NHO: What's a more semantic name for this variable?
+var gameValue = []; // NHO: What's a more semantic name for this variable?
+Array.prototype.boardShuffle = function() { // NHO: In general, we avoid defining methods on datatype's prototype
+  var i = this.length,
+    j, temp;
+  while (--i > 0) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = this[j];
+    this[j] = this[i];
+    this[i] = temp;
+  } // NHO: Fischer-yates random^
+}
+
+function newBoard() {
+  cardFlip = 0;
+  var output = '';
+  gameArray.boardShuffle();
+  for (var i = 0; i < gameArray.length; i++) {
+    output += '<div id="block' + i + '" onclick="gameFlip(this,\'' + gameArray[i] + '\')"></div>'; // NHO: We are we attaching onClick listeners here, instead of how we learned in class?
+    document.getElementById('boardGame').innerHTML = output;
+  }
+
+  function gameFlip(block, val) {
+    if (block.innerHTML == "" && gameValue.length < 2) {
+      block.style.background = '#FFF';
+      block.innerHTML = val;
+      if (gameValue.length == 0) {
+        gameValue.push(val);
+        gameId.push(block.id);
+      } else if (gameValue.length == 1) {
+        gameValue.push(val);
+        gameId.push(block.id);
+        if (gameValue[0] == gameValue[1]) {
+          cardFlip += 2;
+          gameValue = [];
+          gameId = [];
+          if (cardFlip == gameArray.length) {
+            alert("You got all the blocks to match! Starting a new game now...");
+            document.getElementById('boardGame').innerHTML = "";
+            newBoard();
+          }
+        } else {
+          function flipOver() {
+            var matchOne = document.getElementById(gameId[0]);
+            var matchTwo = document.getElementById(gameId[1]);
+            matchOne.style.background = 'no-repeat'; // NHO: What's this line doing?
+            matchOne.innerHTML = "";
+            matchTwo.style.background = 'no-repeat'; // NHO: What's this line doing?
+            matchTwo.innerHTML = "";
+            gameValue = [];
+            gameId = [];
+          }
+
+          setTimeout(flipOver, 700);
+        }
+      }
     }
-}
-
-function newBoard(){
-	cardFlip = 0;
-	var output = '';
-    gameArray.boardShuffle();
-	for(var i = 0; i < gameArray.length; i++){
-		output += '<div id="block'+i+'" onclick="gameFlip(this,\''+gameArray[i]+'\')"></div>';
-	}
-	document.getElementById('boardGame').innerHTML = output;
-}
-
-function gameFlip(block, val){
-	if(block.innerHTML == "" && gameValue.length < 2){
-		block.style.background = '#FFF';
-		block.innerHTML = val;
-		if(gameValue.length == 0){
-			gameValue.push(val);
-			gameId.push(block.id);
-		} 
-		else if(gameValue.length == 1){
-			gameValue.push(val);
-			gameId.push(block.id);
-			if(gameValue[0] == gameValue[1]){
-				cardFlip += 2;
-				gameValue = [];
-            	gameId = [];
-				if(cardFlip == gameArray.length){
-					alert("You got all the blocks to match! Starting a new game now...");
-					document.getElementById('boardGame').innerHTML = "";
-					newBoard();
-				}
-			} else {
-				function flipOver(){
-				    var matchOne = document.getElementById(gameId[0]);
-				    var matchTwo = document.getElementById(gameId[1]);
-				    matchOne.style.background = 'no-repeat';
-            	    matchOne.innerHTML = "";
-				    matchTwo.style.background = 'no-repeat';
-            	    matchTwo.innerHTML = "";
-				    gameValue = [];
-            	    gameId = [];
-				}
-
-				setTimeout(flipOver, 700);
-			}
-		}
-	}
-}
+  }
+  // NHO: Overall JS looks good, but why are you using only Vanilla JS? Also, make sure to check indentation!
+  // I ran your code through Atom's Beautify package to fix spacing, indentation, etc.
